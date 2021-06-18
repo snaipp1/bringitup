3e08ed56ecf98c6b243eb6cf36726e4d15a2a19f
snaipp1 <snaipp1@yandex.by>
2021-06-18 20:42:14 +0300

    feat: add pop-up block

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -15,15 +15,31 @@ export default class Slider {
             this.slideIndex = this.slides.length;
         }
 
-        this.slides.forEach(slide => {
-            slide.classList.add('animated', 'fadeOutUp');
-            slide.classList.remove('fadeInDown');
-            slide.style.display = 'none';
-        });
+        try{
+            this.hanson.style.opacity = '0';
 
-        this.slides[this.slideIndex - 1].classList.add('fadeInDown');
-        this.slides[this.slideIndex - 1].classList.remove('fadeOutUp');
-        this.slides[this.slideIndex - 1].style.display = 'block'; 
+            if (n === 3) {
+                this.hanson.classList.add('animated');
+                setTimeout(() => {
+                    this.hanson.style.opacity = '1';
+                    this.hanson.classList.add('slideInUp');
+                }, 3000);
+            }else {
+                this.hanson.classList.remove('slideInUp');
+            }
+        }catch(e){
+            console.log(e)
+        }finally{  
+            this.slides.forEach(slide => {
+                slide.classList.add('animated', 'fadeOutUp');
+                slide.classList.remove('fadeInDown');
+                slide.style.display = 'none';
+            });
+
+            this.slides[this.slideIndex - 1].classList.add('fadeInDown');
+            this.slides[this.slideIndex - 1].classList.remove('fadeOutUp');
+            this.slides[this.slideIndex - 1].style.display = 'block'; 
+        }
     }
 
     plusSlides (n) {
@@ -31,19 +47,25 @@ export default class Slider {
     }
 
     render () {
-        this.btns.forEach(item => {
-            item.addEventListener('click', () => {
-                this.plusSlides(1);
+        try{
+            this.hanson = document.querySelector('.hanson');
+        }catch(e){
+            console.log(e);
+        } finally{
+            this.btns.forEach(item => {
+                item.addEventListener('click', () => {
+                    this.plusSlides(1);
+                });
+    
+                item.parentNode.previousElementSibling.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    this.slideIndex = 1;
+                    this.showSlides(this.slideIndex);
+                });
             });
-
-            item.parentNode.previousElementSibling.addEventListener('click', (e) => {
-                e.preventDefault();
-                this.slideIndex = 1;
-                this.showSlides(this.slideIndex);
-            });
-        });
-
-        this.showSlides(this.slideIndex);
+    
+            this.showSlides(this.slideIndex);
+        }
     }
 
 
