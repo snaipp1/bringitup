5181a2eff34fd60f0a02ea208c3a4c2727169b2b
snaipp1 <snaipp1@yandex.by>
2021-07-02 21:26:53 +0300

    feat: add playVideo class

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 import Slider from './modules/slider';
+import VideoPlayer from './modules/playVideo';
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new Slider('.page', '.next');
     slider.render();
+
+    const videoPlayer = new VideoPlayer('.showup .play', '.overlay');
+    videoPlayer.init();
 });
