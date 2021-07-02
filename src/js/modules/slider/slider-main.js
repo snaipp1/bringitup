b6a47f401f3a02d4f38a49b97ff44ff6b9adaf57
snaipp1 <snaipp1@yandex.by>
2021-07-02 22:32:35 +0300

    feat: add slider-main class

--- src/js/modules/slider/slider-main.js
+++ src/js/modules/slider/slider-main.js
@@ -1,11 +1,12 @@
-export default class Slider {
+import Slider from "./slider";
+
+export default class MainSlider extends Slider {
+
     constructor (page, btns) {
-        this.page = document.querySelector(page);
-        this.slides = this.page.children;
-        this.btns = document.querySelectorAll(btns);
-        this.slideIndex = 1;
+        super(page, btns);
     }
 
+    
     showSlides (n) {
         if (n > this.slides.length) {
             this.slideIndex = 1;
@@ -67,6 +68,4 @@ export default class Slider {
             this.showSlides(this.slideIndex);
         }
     }
-
-
 }
