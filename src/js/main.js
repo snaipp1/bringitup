8122b92f204b2b93477eddc954652be955509a0d
snaipp1 <snaipp1@yandex.by>
2021-07-03 19:17:33 +0300

    feat: add difference class

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import MainSlider from './modules/slider/slider-main';
 import MiniSlider from './modules/slider/slider-mini';
 import VideoPlayer from './modules/playVideo';
+import Difference from './modules/difference';
 
 window.addEventListener('DOMContentLoaded', () => {
     const mainSlider = new MainSlider({container:'.page', btns:'.next'});
@@ -36,4 +37,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const videoPlayer = new VideoPlayer('.showup .play', '.overlay');
     videoPlayer.init();
+
+    new Difference('.officerold', '.officernew', '.officer__card-item').init();
 });
